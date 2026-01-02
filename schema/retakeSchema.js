c6ed11f6d2051f516620ffcf99a3855f7a7aeb4e
GutsofHyrule <Dresmanns@gmail.com>
2026-01-02 12:31:39 -0500

    update

--- schema/retakeSchema.js
+++ schema/retakeSchema.js
@@ -5,13 +5,13 @@ const RetakeTokenSchema = new mongoose.Schema(
     token: { type: String, required: true, unique: true, index: true },
     email: { type: String, required: true, index: true },
     used: { type: Boolean, default: false },
-    expiresAt: { type: Date, required: true, index: true },
+    expiresAt: { type: Date, required: true },
     usedAt: { type: Date, default: null },
   },
   { timestamps: true }
 );
 
-// TTL cleanup (Mongo will remove docs after expiresAt)
+
 RetakeTokenSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
 
 module.exports = mongoose.model('RetakeToken', RetakeTokenSchema);
